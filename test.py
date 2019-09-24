2a2a818814c380a3824948e7fde1222b945504a4
Joe Monaco <joe@Joes-MacBook-Air.local>
2019-09-24 15:11:20 -0400

    yea

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
+
 from bs4 import BeautifulSoup as BS
 import urllib
 import requests
 
+url = 'http://njcaa.org/sports/bsb/2018-19/div1/teams/arizonawesterncollege?view=lineup'
+result = requests.get(url)
+c = result.content
+soup = BS(c)
+print(soup)
