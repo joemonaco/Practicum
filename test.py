1524797ce23cca1b5a6c7dc6a2144ae61fa96a9e
Joe Monaco <joe@Joes-MacBook-Air.local>
2019-09-24 15:23:48 -0400

    added html file

--- test.py
+++ test.py
@@ -6,5 +6,10 @@ import requests
 url = 'http://njcaa.org/sports/bsb/2018-19/div1/teams/arizonawesterncollege?view=lineup'
 result = requests.get(url)
 c = result.content
-soup = BS(c)
+# soup = BS(c)
+soup = BS(c, 'html.parser')
 print(soup)
+
+htmlFile = open('index.html', 'w')
+htmlFile.write(soup.prettify())
+htmlFile.close()
