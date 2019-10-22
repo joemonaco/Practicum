32d2b2217b1b0770cafa5aaaf1e6c97a8be973fa
Joe Monaco <s1049491@monmouth.edu>
2019-10-22 17:46:32 -0400

    using selenium to make calls

--- test.py
+++ test.py
@@ -2,14 +2,37 @@
 from bs4 import BeautifulSoup as BS
 import urllib
 import requests
+import os
+import re
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 
-url = 'http://njcaa.org/sports/bsb/2018-19/div1/teams/arizonawesterncollege?view=lineup'
+url = 'http://cloud.rapsodo.com/2.1/#/auth/login'
 result = requests.get(url)
 c = result.content
+
+# SELENIUM
+driver = webdriver.Firefox()
+driver.implicitly_wait(30)
+driver.get(url)
+
+driver.find_element_by_id('username').send_keys('ccollazo@monmouth.edu')
+driver.find_element_by_id('password').send_keys('mubaseball')
+driver.find_element_by_class_name('auth-button').click()
+
+url = 'http://cloud.rapsodo.com/2.1/#/team-stats'
+
+driver.get(url)
+time.sleep(10)
+# driver.implicitly_wait(30)
+
 # soup = BS(c)
-soup = BS(c, 'html.parser')
+page_text = driver.page_source.encode('utf-8').decode('ascii', 'ignore')
+soup = BS(page_text, 'html.parser')
 print(soup)
 
+# soup.
 htmlFile = open('index.html', 'w')
 htmlFile.write(soup.prettify())
 htmlFile.close()
