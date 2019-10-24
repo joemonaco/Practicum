bf6081edb644615c1f42cd0d4b66e1e716c8de35
Joe Monaco <s1049491@monmouth.edu>
2019-10-24 12:20:59 -0400

    gets the html file of team stats

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-
 from bs4 import BeautifulSoup as BS
 import urllib
 import requests
@@ -8,7 +7,6 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
-# need to "pip install selenium"
 
 url = 'http://cloud.rapsodo.com/2.1/#/auth/login'
 result = requests.get(url)
@@ -28,6 +26,7 @@ url = 'http://cloud.rapsodo.com/2.1/#/team-stats'
 driver.get(url)
 time.sleep(10)
 
+# Get rid of all ascii characters
 page_text = driver.page_source.encode('utf-8').decode('ascii', 'ignore')
 soup = BS(page_text, 'html.parser')
 
